506ed9b45dc099d9061708c66292612f1c100b7a
Barry Levy <barry.levy@gmail.com>
2017-01-04 18:47:04 -0500

    Complete problems

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -6,6 +6,57 @@ func sayHello() {
 sayHello()
 sayHello()
 
+func greet(person:String) {
+    
+    print("Hello to you, \(person). I hope you're having a great day!")
+    
+}
+
+greet(person: "Bilbo")
+greet(person: "John")
+
+func greet(animal: String) -> String {
+    
+    let greeting = "Hello \(animal), what a cute pet you are."
+    
+    return greeting
+    
+}
+
+let greetingForCat = greet(animal: "Hannah")
+
+print(greetingForCat)
+
+func eat() {
+    
+    print("Eat all the pizza")
+}
+
+eat()
+
+
+func sayHello(person: String, age: Int, greeting: String) -> String {
+    
+    let sentence = "\(person) is \(age) years old, that's great. \(greeting)"
+    
+    
+    return sentence
+    
+}
+
+let greetingForPerson = sayHello(person: "Bilbo", age: 110, greeting: "Hello! Have a wonderful day.")
+
+print(greetingForPerson)
+
+
+
+
+
+
+
+
+
+
 // Uncomment this line to see the error that is printed in the console.
 // (You can uncomment a line by removing the // at the beginning of the line.)
 //print(greeting)
